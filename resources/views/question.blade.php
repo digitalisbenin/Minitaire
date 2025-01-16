d39c6f5cffaa1fe305915720d8b6e7a82b257e10
Kodjo Gagnon GAGLOZOUN <gaglozoungagnon>
2025-01-16 08:59:23 +0100

    fixed new dash

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -60,16 +60,13 @@
 
 
 
-            <
-            <div class="ms-auto mt-3">
-
-            </div>
+          
           </div>
           @foreach($quiz as $value)
           <div class="row">
             <div class="col-12 ">
                 <div class="section-title">
-                      <span class="wow zoomIn" data-wow-delay="0.2s"></span>
+                    
                      <h2 class="wow fadeInUp" data-wow-delay=".4s">Quizz par chapitre</h2>
                     <p class="wow fadeInUp" data-wow-delay=".6s">{{$value->titre}}</p>
                 </div>
