d46fe9cf84998e895c388ca5e0bb67c0950a38c6
Kodjo Gagnon GAGLOZOUN <gaglozoungagnon>
2025-01-20 19:47:50 +0100

    fixed new dash

--- resources/views/admin/cours/create.blade.php
+++ resources/views/admin/cours/create.blade.php
@@ -65,10 +65,7 @@
                                 <label for="courseTitle" class="h5 mb-8 fw-semibold font-heading">Description <span class="text-13 text-gray-400 fw-medium"></span> </label>
                                 <div class="position-relative">
                                     <textarea type="text" class="text-counte placeholder-13 form-control py-11 pe-76" name="description" maxlength="300" id="course" placeholder=""  style="height: 100px;"></textarea>
-                                    {{--  <div class="text-gray-400 position-absolute inset-inline-end-0 top-50 translate-middle-y me-16">
-                                        <span id="current">3</span>
-                                        <span id="maximum">/ 300</span>
-                                    </div>  --}}
+                                    
                                 </div>
                             </div>
                             <div class="col-sm-6">
