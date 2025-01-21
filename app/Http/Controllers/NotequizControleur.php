8951731ead496f243b6c9ec9e335758982c8b4bf
Kodjo Gagnon GAGLOZOUN <gaglozoungagnon>
2025-01-21 13:13:21 +0100

    fixed new dash

--- app/Http/Controllers/NotequizControleur.php
+++ app/Http/Controllers/NotequizControleur.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class NomDuControleur extends Controller
+class NotequizControleur extends Controller
 {
     /**
      * Display a listing of the resource.
